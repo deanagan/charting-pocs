1a703eab7d3667b6c96c11fdc5b65682682f720e
Dean Agan <agandfr@gmail.com>
2020-07-12 20:44:26 +1000

    Add recipe repository annotation

--- src/main/java/com/recipes/recipes/model/RecipeRepository.java
+++ src/main/java/com/recipes/recipes/model/RecipeRepository.java
@@ -2,7 +2,9 @@ package com.recipes.recipes.model;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
 
