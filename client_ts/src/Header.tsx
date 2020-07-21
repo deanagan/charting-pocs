8cab2a3c378a59d57846afdef4f7d36e0e37a859
Dean Agan <agandfr@gmail.com>
2020-07-21 20:17:54 +1000

    Add user icon to header. Leave the styling for later.

--- client_ts/src/Header.tsx
+++ client_ts/src/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { UserIcon } from './Icons';
 
 export const Header = () => (
   <div>
     {' '}
     <a href="./">Recipes</a>{' '}
+    <input type="text" placeholder="Search..." />
+    <a href="./signin">
+        <UserIcon />
+        <span>Sign In</span></a>
+
   </div>
 );
