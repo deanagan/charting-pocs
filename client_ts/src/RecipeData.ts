64927aea0988228ab7cf2ea9e4019c247ce1be89
Dean Agan <agandfr@gmail.com>
2020-07-22 18:03:18 +1000

    Add interface and dummy data

---
+++ client_ts/src/RecipeData.ts
@@ -0,0 +1,32 @@
+export interface RecipeData {
+    id: number;
+    name: string;
+    description: string;
+    author: string;
+}
+
+const recipes: RecipeData[] = [
+{
+    id: 1,
+    name: "Hummus",
+    description: "A simple dip with chickpeas",
+    author: "Joe"
+},
+{
+    id: 2,
+    name: "No bake fudge",
+    description: "A no bake brownie fudge!",
+    author: "Greg"
+},
+{
+    id: 3,
+    name: "Eggplant Salad",
+    description: "A salad for all ages",
+    author: "Bob"
+},
+];
+
+
+export const geRecipes = (): RecipeData[] => {
+    return recipes;
+};
