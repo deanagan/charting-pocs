e634e20016fc27166e21d12888e4ae8d40317d5f
Dean Agan <agandfr@gmail.com>
2020-07-26 09:54:54 +1000

    Add recipe list

---
+++ client_ts/src/RecipeList.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { FC } from 'react';
+import { css, jsx } from '@emotion/core';
+import { gray5, accent2 } from './Styles';
+import { RecipeData } from './RecipeData';
+
+interface Props {
+    data: RecipeData[];
+}
+
+export const RecipeList: FC<Props> = ({data}) => (
+    <ul
+        css={css`
+            list-style: none;
+            margin: 10px 0 0 0 ;
+            padding: 0px 20px;
+            background-color: #fff;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+            border-top: 3px solid ${accent2};
+            box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+        `}
+    >
+        {data.map(recipe => (
+        <li
+            css={css`
+                border-top: 1px solid ${gray5};
+                :first-of-type {
+                    border-top: none;
+                }
+            `}
+        >
+            key={recipe.id}
+            css={css`
+                border-top: 1px solid ${gray5};
+                :first-of-type {
+                  border-top: none;
+                }
+            `}
+        </li>
+        ))}
+    </ul>
+);
