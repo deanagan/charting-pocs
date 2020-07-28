c88d19ffda28355582fef01d1ad61b4a67dcce1b
Dean Agan <agandfr@gmail.com>
2020-07-28 18:06:55 +1000

    Wrap homepage in a page component and use child prop

--- client_ts/src/HomePage.tsx
+++ client_ts/src/HomePage.tsx
@@ -3,35 +3,30 @@ import { css, jsx } from '@emotion/core';
 import { AddRecipeButton } from './Styles';
 import { RecipeList } from './RecipeList';
 import { getRecipes } from './RecipeData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div
+  <Page>
+    <div
     css={css`
       margin: 50px auto 20px auto;
       padding: 30px 20px;
       max-width: 600px;
     `}
   >
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+  <div
+    css={css`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     `}
-    >
-      <h2
-        css={css`
-          font-size: 15px;
-          font-weight: bold;
-          margin: 10px 0px 5px;
-          text-align: center;
-          text-transform: uppercase;
-        `}
-      >
-        Recipes
-      </h2>
+  >
+      <PageTitle>Recipes</PageTitle>
       <AddRecipeButton>Add Recipe</AddRecipeButton>
-    </div>
-    <RecipeList data={getRecipes()} />
+
   </div>
+  <RecipeList data={getRecipes()} />
+  </div>
+  </Page>
 );
