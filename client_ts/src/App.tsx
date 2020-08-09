e6a2bf48a50947cc53dc186e2eae84e52622b520
Dean Agan <agandfr@gmail.com>
2020-08-09 19:58:59 +1000

    Changed recipes as home for project to avoid gh pages conflict.

--- client_ts/src/App.tsx
+++ client_ts/src/App.tsx
@@ -24,8 +24,7 @@ const App: React.FC = () => {
       <Header />
       <Switch>
       <Redirect from='/home' to='/' />
-      <Redirect exact from='/recipes' to='/' />
-      <Route exact path='/' component={HomePage} />
+      <Route exact path='/recipes' component={HomePage} />
       <Route path='/search' component={SearchRecipePage} />
       <Route path='/add' component={AddRecipePage} />
       <Route path='/signin' component={SignInPage} />
