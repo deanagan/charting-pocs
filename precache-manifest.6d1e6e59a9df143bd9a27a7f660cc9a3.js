858cbf88aaf170d970bf4336364fb52a9b77e04e
Dean Agan <agandfr@gmail.com>
2020-08-09 19:22:39 +1000

    Updates

---
+++ precache-manifest.6d1e6e59a9df143bd9a27a7f660cc9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bae1ddad18604f033cda3d795b2427",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "3d524d9143165c12f553",
+    "url": "/recipes/static/css/main.5b7ca5c2.chunk.css"
+  },
+  {
+    "revision": "77ca72bc2c5c32d8b768",
+    "url": "/recipes/static/js/2.f17700f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipes/static/js/2.f17700f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d524d9143165c12f553",
+    "url": "/recipes/static/js/main.ee057abc.chunk.js"
+  },
+  {
+    "revision": "f6d6541812b6c3f96e7b",
+    "url": "/recipes/static/js/runtime-main.e17df5ca.js"
+  },
+  {
+    "revision": "e6aaf4306eedf8e1bb73d44846ebc13d",
+    "url": "/recipes/static/media/user.e6aaf430.svg"
+  }
+]);
