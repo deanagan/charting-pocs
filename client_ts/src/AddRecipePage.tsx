d336aa418d33e794dda7cfcebe472f30fc7d041a
Dean Agan <agandfr@gmail.com>
2020-08-18 22:24:59 +1000

    adding dummy form

--- client_ts/src/AddRecipePage.tsx
+++ client_ts/src/AddRecipePage.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Page } from './Page';
+import { Form } from './Form';
+import { Field } from './Field';
 
-export const AddRecipePage = () => <Page title="Add a Recipe" />;
+export const AddRecipePage = () => (
+  <Page title="Add a Recipe">
+    <Form submitCaption="Submit Code Recipe">
+      <Field name="title" label="Title" />{' '}
+      <Field name="content" label="Content" type="TextArea" />{' '}
+    </Form>
+  </Page>
+);
 
 export default AddRecipePage;
