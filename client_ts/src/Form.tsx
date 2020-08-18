0b55a67761fdfa59bc728229ce2f64bbc8fd79a7
Dean Agan <agandfr@gmail.com>
2020-08-18 22:11:47 +1000

    More work on field and form to use context

--- client_ts/src/Form.tsx
+++ client_ts/src/Form.tsx
@@ -3,11 +3,18 @@ import { AddRecipeButton, gray5, gray6 } from './Styles';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 
-
-
 export interface Values {
-  [key: string]: string;
+  [key: string]: any;
 }
+
+interface FormContextProps {
+  values: Values;
+  setValue?: (fieldName: string, value: any) => void;
+}
+
+export const FormContext = createContext<FormContextProps>({
+  values: {},
+});
 
 interface Props {
   submitCaption?: string;
@@ -17,31 +24,39 @@ export const Form: FC<Props> = ({ submitCaption, children }) => {
   const [values, setValues] = useState<Values>({});
 
   return (
-    <form noValidate={true}>
-      {' '}
-      <fieldset
-        css={css`
-          margin: 10px auto 0 auto;
-          padding: 30px;
-          width: 350px;
-          background-color: ${gray6};
-          border-radius: 4px;
-          border: 1px solid ${gray5};
-          box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
-        `}
-      >
-        {children}
-        <div
+    <FormContext.Provider
+      value={{
+        values,
+        setValue: (fieldName: string, value: any) => {
+          setValues({ ...values, [fieldName]: value });
+        },
+      }}
+    >
+      <form noValidate={true}>
+        {' '}
+        <fieldset
           css={css`
-            margin: 30px 0px 0px 0px;
-            padding: 20px 0px 0px 0px;
-            border-top: 1px solid ${gray5};
+            margin: 10px auto 0 auto;
+            padding: 30px;
+            width: 350px;
+            background-color: ${gray6};
+            border-radius: 4px;
+            border: 1px solid ${gray5};
+            box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
           `}
         >
-
-          <AddRecipeButton type="submit"> {submitCaption} </AddRecipeButton>
-        </div>
-      </fieldset>
-    </form>
+          {children}
+          <div
+            css={css`
+              margin: 30px 0px 0px 0px;
+              padding: 20px 0px 0px 0px;
+              border-top: 1px solid ${gray5};
+            `}
+          >
+            <AddRecipeButton type="submit"> {submitCaption} </AddRecipeButton>
+          </div>
+        </fieldset>
+      </form>
+    </FormContext.Provider>
   );
 };
