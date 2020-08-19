74a0948860092bbe6182dd682f1e56707e5ce223
Dean Agan <agandfr@gmail.com>
2020-08-19 21:50:12 +1000

    Add a couple more fields to match recipes

--- client_ts/src/AddRecipePage.tsx
+++ client_ts/src/AddRecipePage.tsx
@@ -6,8 +6,10 @@ import { Field } from './Field';
 export const AddRecipePage = () => (
   <Page title="Add a Recipe">
     <Form submitCaption="Submit Code Recipe">
-      <Field name="title" label="Title" />{' '}
-      <Field name="content" label="Content" type="TextArea" />{' '}
+      <Field name="title" label="Title" />
+      <Field name="content" label="Content" type="TextArea" />
+      <Field name="originator" label="By" />
+      <Field name="sourcecode" label="Snippet" type="TextArea" />
     </Form>
   </Page>
 );
