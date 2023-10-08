ebd9909ad3a461b48dd72b1439bb9b4a6a73c3ae
Dean Agan <agandfr@gmail.com>
2023-10-08 20:38:56 +1100

    Clear if over ten lines

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,8 +9,17 @@ formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 # Specify the file path
 file_path = "scraper/progress.txt"
 
-# Write the formatted time to the file
-with open(file_path, "a") as file:
-    file.write(formatted_time)
+isNeedsClear = False
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+    if len(lines) > 10:
+        isNeedsClear = True
 
-print("Time written to", file_path)
+if isNeedsClear:
+    with open(file_path, 'w') as file:
+        pass  # This does nothing, but the file is now empty
+else:
+    # Write the formatted time to the file
+    with open(file_path, "a") as file:
+        lines = file.readlines()
+        file.write(formatted_time)
