436c34431fd713920ff2af9c6f091b7e4fe015c4
Dean Agan <agandfr@gmail.com>
2024-08-07 21:28:01 +1000

    WIP linechart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import BarChart from './BarChart';
+import LineChart from './LineChart';
 import './App.css';
+import { data } from './data';
 
 const App: React.FC = () => (
     <div className="App">
         <h1 className="title">Pokemon Base Stat Chart</h1>
         <BarChart />
+        <LineChart data={data} />
     </div>
 );
 
---
+++ src/data.ts
@@ -0,0 +1,110 @@
+export const data = [
+    {
+      id: 'japan',
+      color: 'hsl(348, 70%, 50%)',
+      data: [
+        {
+          x: 'plane',
+          y: 175,
+        },
+        {
+          x: 'helicopter',
+          y: 50,
+        },
+        {
+          x: 'boat',
+          y: 150,
+        },
+        {
+          x: 'train',
+          y: 30,
+        },
+        {
+          x: 'subway',
+          y: 95,
+        },
+        {
+          x: 'bus',
+          y: 60,
+        },
+        {
+          x: 'car',
+          y: 85,
+        },
+        {
+          x: 'moto',
+          y: 60,
+        },
+        {
+          x: 'bicycle',
+          y: 70,
+        },
+        {
+          x: 'horse',
+          y: 60,
+        },
+        {
+          x: 'skateboard',
+          y: 80,
+        },
+        {
+          x: 'others',
+          y: 40,
+        },
+      ],
+    },
+    {
+      id: 'france',
+      color: 'hsl(208, 70%, 50%)',
+      data: [
+        {
+          x: 'plane',
+          y: 30,
+        },
+        {
+          x: 'helicopter',
+          y: 85,
+        },
+        {
+          x: 'boat',
+          y: 160,
+        },
+        {
+          x: 'train',
+          y: 110,
+        },
+        {
+          x: 'subway',
+          y: 90,
+        },
+        {
+          x: 'bus',
+          y: 75,
+        },
+        {
+          x: 'car',
+          y: 50,
+        },
+        {
+          x: 'moto',
+          y: 60,
+        },
+        {
+          x: 'bicycle',
+          y: 70,
+        },
+        {
+          x: 'horse',
+          y: 85,
+        },
+        {
+          x: 'skateboard',
+          y: 45,
+        },
+        {
+          x: 'others',
+          y: 75,
+        },
+      ],
+    },
+  ];
